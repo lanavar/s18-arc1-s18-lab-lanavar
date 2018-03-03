226b51c7193676bc4d9c21663ba6772ba8f6e5ae
Lenin Navar <lanavar@miners.utep.edu>
2018-03-02 19:45:11 -0700

    Updated file. Added debug and option function to main page.

--- lab1arc.c
+++ lab1arc.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXBUF 100    /* Maximum size of the buffer */
+#define MAXBUF 100       /* Maximum size of the buffer */
+#define MAXSTRING 1024   /* Maximum lenght of string */
+#define DEBUG 1     /* Activating debug */
+#define dbprint    /* Debugg print command */
+#ifdef DEBUG
+#define dbprint printf
+#else dbprint /
+#endif
+
+void option(int);
 
 /* Main file for the program, this is the interface where the user will be asked to select what they want to do.
    this is part of lab1 for acr1 class written by Lenin Navar */
@@ -10,7 +19,7 @@ int main()
   char element;                // Element to find out what 
   int choice;                  // int for the choice entered by user
   char buf[MAXBUF];               // Buffer for receiving elements
-
+  
 
   /* Loop for selecting the choice by the customer */
   while(choice!=9){
@@ -28,9 +37,71 @@ int main()
     /* Get information from user */
     if(fgets(buf, sizeof(buf), stdin))
       choice = atoi(buf);
-    
-
-    printf("%c, char %d num choice for user \n", choice, choice);
+ 
+    dbprint("%d num choice for user \n", choice, choice);
+    option(choice);
   }
   return 0;
 }
+/* Method for processing the option selected by the user */
+void option(int sel){
+  char *value = malloc(MAXSTRING);
+  printf("You chose: %d \n", sel);
+
+  switch(sel){
+  case 1:{
+    printf("Please enter the name of the employee to add: ");
+    fgets(value, MAXSTRING, stdin);
+    //bstAdd(bst, value);
+    //printf("%s entered into database. \n", value);
+    dbprint("It was a valid choice. Name to be stored is %s \n", value);
+    break;
+  }
+  case 2:{
+    printf("Please enter the name of the employee to delete: ");
+    fgets(value, MAXSTRING, stdin);
+    //bstRemove(bst, value);
+    //printf("%s removed from the database. \n", value);
+    dbprint("It was a valid choice. Name to be removed is %s \n", value);
+    break;
+  }
+  case 3:{
+    printf("Here is the list of the employee names: ");
+    //bstPrint(bst -> head);
+    //printf(" \n");
+    dbprint("It was a valid choice. List printed.\n", value);
+    break;
+  }
+  case 4:{
+    printf("Please enter the name of the file to read: ");
+    fgets(value, MAXSTRING, stdin);
+    //bstReadFile(bst, value);
+    //printf("The information from file %s has been saved. \n", value);
+    dbprint("It was a valid choice. File to be read from: %s \n", value);
+    break;
+  }
+  case 5:{
+    printf("Please enter the name of the file to save to: ");
+    fgets(value, MAXSTRING, stdin);
+    //bstWrite(bst, value);
+    //printf("The database was saved into file: %s. \n", value);
+    dbprint("It was a valid choice. File to be saved to %s \n", value);
+    break;
+  }
+  case 9:{
+    printf("Exiting program. Thank You. \n");
+    dbprint("It was a valid choice. Exiting. \n");
+    break;
+  }
+  default: {
+    printf("%d is an invalid selection. Please enter a valid one. \n", sel);
+    dbprint("Invalid selection. \n");
+    break;
+  }
+  }
+}
+
+
+
+
+
